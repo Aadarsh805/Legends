7addc70ce45ff4d4c6bba3af9316276623bdfa36
manish bisht <manish205770@keshav.du.ac.in>
2023-01-28 01:11:41 +0530

    feat: navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,12 @@ import Head from "next/head";
 import Image from "next/image";
 import CardSection from "../components/CardSection";
 import MainScreen from "../components/MainScreen";
+import { Navbar } from "../components/Navbar";
 
 const Home: NextPage = () => {
   return (
     <div className="h-screen bg-black">
+      <Navbar />
       <MainScreen />
       <CardSection />
     </div>
